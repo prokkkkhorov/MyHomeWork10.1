20ef5f74692e477048e0aba7841ba687c04ab412
Egor Prokhorov <egorprohoroff@yandex.ru>
2025-01-23 12:56:09 +0400

    code reduction and add get_date function

--- src/widget.py
+++ src/widget.py
@@ -10,21 +10,23 @@
 
 def mask_account_card(data_card: str) -> str:
     """" Функция для зашифровки данных карт и счетов """
-    name_of_card = ''
-    number_of_card = ''
-    for alpha in data_card:
-        if alpha.isalpha():
-            name_of_card += alpha
-    for digit in data_card:
-        if digit.isdigit():
-            number_of_card += digit
+    name_of_card = "".join(alpha for alpha in data_card if alpha.isalpha())
+    number_of_card = "".join(digit for digit in data_card if digit.isdigit())
 
     if name_of_card != "Счет":
         return f'{name_of_card} {number_of_card[0:4]} {number_of_card[4:6]}** **** {number_of_card[-4:]}'
-
-    if name_of_card == "Счет":
+    elif name_of_card == "Счет":
         return f'{name_of_card} **{number_of_card[-4:]}'
 
 
 if __name__ == "__main__":
     print(mask_account_card('Счет 73654108430135874305'))
+
+
+def get_date(date: str) -> str:
+    """ Функция для перевода даты из вида ISO 8601 в привычный европейский"""
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+
+
+if __name__ == "__main__":
+    print(get_date('2024-03-11T02:26:18.671407'))
