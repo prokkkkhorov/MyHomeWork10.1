813bbceaf3ff95446d2f1c28994ff95d7738f1a1
Egor Prokhorov <egorprohoroff@yandex.ru>
2025-02-03 11:09:51 +0400

    add processing.py

--- main.py
+++ main.py
@@ -8,8 +8,16 @@
 # Visa Gold 5999414228426353
 # Счет 73654108430135874305
 
+list_of_dict = [
+{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import get_date, mask_account_card
+from src.processing import filter_by_state
 
 if __name__ == "__main__":
     masked_card = get_mask_card_number(7000792289606361)
@@ -23,3 +31,7 @@ if __name__ == "__main__":
 
     print(masked_account_card)
     print(reform_date)
+
+    filter_by_state_main = filter_by_state(list_of_dict)
+
+    print(filter_by_state_main)
