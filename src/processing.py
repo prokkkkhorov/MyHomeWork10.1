4bcded2e00d00f34229b2f86e05b6174b247996a
Egor Prokhorov <egorprohoroff@yandex.ru>
2025-02-05 00:08:32 +0400

    add Docstrings

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,12 @@
 def filter_by_state(my_list: list, state='EXECUTED') -> list:
+    """
+    Функция для сортировки списка словарей по статусу
+    """
     return [elem for elem in my_list if elem['state'] == state]
 
 
 def sort_by_date(my_list: list, reverse=False) -> list:
+    """
+    Функция для сортировки списка словарей по дате
+    """
     return sorted(my_list, key=lambda x: x['date'], reverse=reverse)
