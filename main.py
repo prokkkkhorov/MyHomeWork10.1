ccbad343443a40b050fa2c4ef0d42dbb3fdb0c44
Egor Prokhorov <egorprohoroff@yandex.ru>
2025-02-10 15:07:01 +0400

    add docstrings

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
+from src.processing import filter_by_state, sort_by_date
+
 # Примеры входных данных для проверки функции
 # Maestro 1596837868705199
 # Счет 64686473678894779589
@@ -9,15 +13,11 @@
 # Счет 73654108430135874305
 
 list_of_dict = [
-{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
 ]
-
-from src.masks import get_mask_account, get_mask_card_number
-from src.widget import get_date, mask_account_card
-from src.processing import filter_by_state, sort_by_date
 
 if __name__ == "__main__":
     masked_card = get_mask_card_number(7000792289606361)
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     print(reform_date)
 
     filter_by_state_main = filter_by_state(list_of_dict, state='EXECUTED')
-    sorted_date = sort_by_date(list_of_dict)
+    sorted_date = sort_by_date(list_of_dict, reverse=False)
 
     print(filter_by_state_main)
     print(sorted_date)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 @pytest.fixture()
 def card_number_func():
     return '7000 79** **** 6361'
@@ -14,18 +13,3 @@ def mask_account_func():
 @pytest.fixture()
 def get_date_func():
     return '11.03.2024'
-
-
-@pytest.fixture()
-def filter_by_state_func1():
-    return [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-    ]
-
-@pytest.fixture()
-def filter_by_state_func2():
-    return [
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ]
