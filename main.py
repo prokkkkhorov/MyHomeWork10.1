595661fb322272a95a9e04c6b3c8b04449672818
Egor Prokhorov <egorprohoroff@yandex.ru>
2025-02-14 16:44:32 +0400

    add filter_by_currency, transaction_descriptions, card_number_generator

--- main.py
+++ main.py
@@ -15,9 +15,90 @@ list_of_dict = [
 {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
 ]
 
+transactions = (
+    [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }
+    ]
+)
+
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import get_date, mask_account_card
 from src.processing import filter_by_state, sort_by_date
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
 if __name__ == "__main__":
     masked_card = get_mask_card_number(7000792289606361)
@@ -26,14 +107,38 @@ if __name__ == "__main__":
     print(masked_card)
     print(masked_account)
 
+    print("__________")
+
     masked_account_card = mask_account_card('Visa Platinum 8990922113665229')
     reform_date = get_date('2024-03-11T02:26:18.671407')
 
     print(masked_account_card)
     print(reform_date)
 
+    print("__________")
+
     filter_by_state_main = filter_by_state(list_of_dict)
     sorted_date = sort_by_date(list_of_dict)
 
     print(filter_by_state_main)
     print(sorted_date)
+
+    print("__________")
+
+    usd_transactions = filter_by_currency(transactions, "USD")
+    for _ in range(2):
+        print(next(usd_transactions))
+
+    print("__________")
+
+    descriptions = transaction_descriptions(transactions)
+    for _ in range(5):
+        print(next(descriptions))
+
+    print("__________")
+
+    for card_number in card_number_generator(1, 5):
+        print(card_number)
+
+    print("__________")
+
